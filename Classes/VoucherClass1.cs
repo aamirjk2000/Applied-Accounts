cd5ee9cb41b99df4dfd9b2caa4a41b06eb330e30
Muhammad Aamir Jahangir <aamir@jahangir.com>
2021-02-13 13:53:08 +0500

    AMCORP - 13-FEB-2021

---
+++ Classes/VoucherClass1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applied_Accounts.Classes
+{
+    interface IVoucherClass1
+    {
+        void Save();
+        void Save(DataRow _Row);
+    }
+
+
+    class VoucherClass1 : IVoucherClass1
+    {
+        private DataTable tb_Voucher;
+        private DataTable tb_Voucher_Delete;
+        private DataTable tb_GridData;
+        private DataRow MyRow;
+        private Dictionary<string, string> MyVoucher = new Dictionary<string, string>();
+
+        //===============================================================================================
+
+
+
+        VoucherClass1()
+        {
+            
+
+        }
+
+        VoucherClass1(string _VouNo)
+        {
+
+
+        }
+
+
+        #region Save
+
+        public void Save()
+        {
+            Save(MyRow);
+        }
+
+        public void Save(DataRow _Row)
+        {
+
+        }
+
+        #endregion
+  
+
+
+    
+    
+    }       // END Class
+}           // END NameSpace
